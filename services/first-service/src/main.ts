859f4ac0e9b6d72cacc1c54a28f46532e095ccdb
Ethan Goode <ethan.goode@wsu.edu>
2026-02-26 17:33:55 -0800

    Add our first basic service. This will be extended later

---
+++ services/first-service/src/main.ts
@@ -0,0 +1,29 @@
+import express, { response } from "express";
+import type {Request, Response, NextFunction} from "express";
+
+const app = express();
+app.use(express.json())
+
+let HOST: string;
+let PORT: number;
+if(process.env.PRODUCTION === undefined) {
+	console.log("Not in production");
+	HOST = "127.0.0.1";
+	PORT = 3000;
+} else {
+	console.log("In production");
+	HOST = "10.0.0.6";
+	PORT = 3000;
+};
+
+app.get('/', (req: Request, res: Response) => {
+    res.send("Typescript with express!");
+});
+
+app.get('/auth', (req: Request, res: Response) => {
+	res.send("Welcome to the Auth endpoint");
+})
+
+app.listen(PORT, HOST, () => {
+    console.log(`Server is running on ${HOST}:${PORT}`);
+});
